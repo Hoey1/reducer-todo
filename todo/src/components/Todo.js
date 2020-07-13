1962217815b89f9abdcc57e8d540f0001ad4b3d1
Joey <joey@xm8.ninja>
2020-07-13 15:47:19 -0400

    changing it up, adding tags as main stretch goal after finding out its fairly simple, reorganizing jsx flow

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,15 +1,24 @@
-import React, { useReducer, useState } from "react";
-import { initialState, reducer } from "../reducers/reducer";
+import React from "react";
+import "../Todo.css";
+import { Tag } from "./Tag";
 
-const Todo = () => {
-  const [newTodo, setNewTodo] = useState("");
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const handleChanges = (e) => {
-    setNewTitleText(e.target.value);
-  };
-
-  return <div></div>;
+export const Todo = (props) => {
+  return (
+    <div className="container">
+      <div className="todo-list">
+        <div
+          onClick={props.toggleToDo}
+          className={props.todo.completed ? "complete" : ""}
+          id={props.todo.id}
+        >
+          {props.todo.item}
+        </div>
+        <div>
+          {props.todo.tags.map((tag) => {
+            return <Tag tag={tag} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
-
-export default Todo;
--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -1,14 +1,40 @@
+import React from "react";
+
 export const initialState = {
   item: "Learn about reducers",
   completed: false,
   id: 3892987589,
+  tags: ["reducers", "react", "lambda"],
 };
 //
 // Reducer always takes in (state, action)
 export const reducer = (state, action) => {
+  const newState = [...state];
+
   switch (action.type) {
+    case "ADD_TODO":
+      return [
+        ...state,
+        {
+          item: action.newItem,
+          completed: false,
+          id: Date.now(),
+          tags: action.tags.split(","),
+        },
+      ];
+    case "TOGGLE_TODO":
+      newState[action.payload] = {
+        ...newState[action.payload],
+        completed: !newState[action.payload].completed,
+      };
+      return newState;
+    case "CLEAR_COMPLETED":
+      const filteredArray = newState.filter((todo) => {
+        return !todo.completed;
+      });
+      return filteredArray;
     default:
-      return state;
+      return newState;
   }
 };
 
